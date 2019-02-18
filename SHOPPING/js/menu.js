b2a189af76360c7731dd5ae3a103f8fcd5d9b7b1
eikiychang-dell <eikiybaby@gmail.com>
2019-02-18 17:25:55 +0800

    Add video & img-info

--- SHOPPING/js/menu.js
+++ SHOPPING/js/menu.js
@@ -1,6 +1,5 @@
 
  $(function(){
-
   $(".main_contents .sideMenu ul li.btn_more").click(function(){
         $(this).find("ul.subMenu").slideToggle({duration: 'slow', queue: false});
     return false;
@@ -29,3 +28,45 @@
   }
   return false;
  });
+
+
+//點右邊top
+ $(function(){
+  $(window).scroll(function(){
+    if( $(window).scrollTop() > 400 ){
+      $(".shoppingcar_icon a.btn_go_up").stop(true,false).animate({opacity:1},);
+    }else{
+      $(".shoppingcar_icon a.btn_go_up").stop(true,false).animate({opacity:0},);
+    }
+  });
+  $(".shoppingcar_icon a.btn_go_up").click(function goTop(){
+    $("html,body").stop(true,false).animate({scrollTop:0},900);
+  });
+  return false;
+ });
+
+// 分享按鈕 + 動態
+$(function() {
+    var moved = 0;
+    var body = $("body");
+    var width = body.width()
+    $(document).on('click', '.shoppingcar_icon .btn_share', function(e){
+      e.preventDefault();
+        if (moved == 0) {
+          $(this).find("ul.share_web_list").animate({'right': '0px'}, 0.5);
+          if(width < 499) {
+              $(this).find("ul.share_web_list").animate({'right': '50px', 'opacity':'1'});
+          } else {
+              $(this).find("ul.share_web_list").animate({'right': '60px', 'opacity':'1'});
+          }
+          moved = 1;
+        }
+        else{
+            moved = 0;
+            $(this).find("ul.share_web_list").animate({'right': '0px', 'opacity':'0'});
+            $(this).find("ul.share_web_list").animate({'right': '-200px'});
+        }
+    });
+});
+
+
